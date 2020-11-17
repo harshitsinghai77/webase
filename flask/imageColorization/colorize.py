e8864c23e37560747f7b227a2ca23130a413d44a
Harshit Singhai <harshitsinghai77@gmail.com>
2020-11-17 14:32:13 +0530

    refactored

--- flask/imageColorization/colorize.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def imageColorization():
-    url = 'http://iizuka.cs.tsukuba.ac.jp/projects/colorization/web/'
-    img_path = 'imageColorization/uploads/colorize.jpg'
-    files = {'file': (img_path, open(img_path, 'rb'),'image/jpg')}
-    response = requests.post(url, files=files)
-    return response.content
--- flask/nsfw/nsfw.py
+++
@@ -1,8 +0,0 @@
-import requests
-import Algorithmia
-
-def nsfw_predict(url):
-
-    client = Algorithmia.client('simWe97NtJhjsrvkB0qrvKXPijo1')
-    algo = client.algo('spullara/YahooOpenNSFW/0.1.1')
-    return algo.pipe(url).result
